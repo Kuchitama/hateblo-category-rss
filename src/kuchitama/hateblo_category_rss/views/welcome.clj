b12ffe882f66a984511ce47eea670f3a70109296
Kuchitama <kiyo.kunihira@gmail.com>
2013-01-29 02:05:46 +0900

    よけいなviewを削除

--- src/kuchitama/hateblo_category_rss/views/welcome.clj
+++
@@ -1,8 +0,0 @@
-(ns kuchitama.hateblo-category-rss.views.welcome
-  (:require [kuchitama.hateblo-category-rss.views.common :as common]
-            [noir.content.getting-started])
-  (:use [noir.core :only [defpage]]))
-
-(defpage "/welcome" []
-         (common/layout
-           [:p "Welcome to hateblo-category-rss"]))
